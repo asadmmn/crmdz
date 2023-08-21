649f360095ef5d00cd9c3f5375ab909cccc7a4c8
asadmmn <74127844+asadmmn@users.noreply.github.com>
2023-08-20 23:27:15 -0700

    yeah cmt it now

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,9 +2,6 @@
 <x-slot name="header">
     <h2 class="font-semibold text-xl bg-white text-gray-800 leading-tight">
         
-            <a href="{{ route('users.index') }}">Users management</a> <br>
-            <a href="{{ route('roles.index') }}">Roles management</a> <br>
-            <a href="{{ route('products.index') }}">Jobs management</a>
                 
     </h2>
 </x-slot>
@@ -14,9 +11,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in! now congratulations for implementing auto deployment  ") }}
+                    <h1>
+                    {{ __("Welcome to CodeZeroes-CRM here you can find perfect job for you ") }}
 
-
+                    </h1>
                 </div>
             </div>
         </div>
