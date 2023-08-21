4633903e61e4354aac5df846761fb73d02cc25bd
asadmmn <74127844+asadmmn@users.noreply.github.com>
2023-08-21 02:41:59 -0700

    alright

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -35,7 +35,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->intended(RouteServiceProvider::HOME);
    
         }
-       //  }
+    }
 
     /**
      * Destroy an authenticated session.
