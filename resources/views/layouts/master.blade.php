87f783df4b47ad43df3a88fbae8d21978cfac645
asadmmn <74127844+asadmmn@users.noreply.github.com>
2023-08-20 22:57:40 -0700

    ok comit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,23 +35,7 @@
                             <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
                             @endrole
                             <li><a class="nav-link" href="{{ route('products.index') }}">Manage jobs</a></li>
-                            <li class="ms-3 nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ ('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            @include('layouts.navigation')
                         @endguest
                     </ul>
                 </div>
