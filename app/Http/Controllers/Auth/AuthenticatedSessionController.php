fc8847ab16e6032145699df12767fa2e5c549f2f
asadmmn <74127844+asadmmn@users.noreply.github.com>
2023-08-21 00:58:02 -0700

    CMMT

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,9 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect()->route('products.index');
+   
+       // return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
